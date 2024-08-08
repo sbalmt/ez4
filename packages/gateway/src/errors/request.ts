e6ead6861f2f6168a4495c1af958d02886a9fe18
Silas B <silas@balmante.eti.br>
2024-08-08 13:57:09 -0300

    refactor: change error class order

--- packages/gateway/src/errors/request.ts
+++ packages/gateway/src/errors/request.ts
@@ -1,16 +1,16 @@
 import { IncorrectTypeError, InvalidTypeError } from '@ez4/common/library';
 
+export class InvalidRequestTypeError extends InvalidTypeError {
+  constructor(fileName?: string) {
+    super('Invalid route request', undefined, 'Http.Request', fileName);
+  }
+}
+
 export class IncorrectRequestTypeError extends IncorrectTypeError {
   constructor(
     public requestType: string,
     fileName?: string
   ) {
     super('Incorrect route request', requestType, 'Http.Request', fileName);
-  }
-}
-
-export class InvalidRequestTypeError extends InvalidTypeError {
-  constructor(fileName?: string) {
-    super('Invalid route request', undefined, 'Http.Request', fileName);
   }
 }
