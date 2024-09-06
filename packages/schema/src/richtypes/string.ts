dc1d0cb4fa5eaddc2e4c8a4c691dcfccfe12d5c4
Silas B <silas@balmante.eti.br>
2024-09-06 15:58:15 -0300

    refactor: make regex schema name mandatory

--- packages/schema/src/richtypes/string.ts
+++ packages/schema/src/richtypes/string.ts
@@ -7,7 +7,7 @@ export namespace String {
   /**
    * A string matching regex.
    */
-  export type Regex<_Pattern extends string, _Name extends string = 'regex'> = string;
+  export type Regex<_Pattern extends string, _Name extends string> = string;
 
   /**
    * A string within a minimum length constraint.
