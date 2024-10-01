7de43fd6f98c278f6433c6636df79ba60499c621
Silas B <silas@balmante.eti.br>
2024-10-01 12:25:20 -0300

    feat: log broken json requests

--- packages/aws-gateway/src/runtime/errors.ts
+++ packages/aws-gateway/src/runtime/errors.ts
@@ -1,16 +1,17 @@
 import { HttpError } from '@ez4/gateway';
 
 /**
- * Get a stringified JSON error for the given HTTP error.
+ * Get a JSON error response for the given HTTP error.
  *
- * @param message Error message.
- * @param details Optional error details.
- * @returns Returns a stringified JSON error containing the given `message` and `details`.
+ * @returns Returns an error response containing the given `status`, `message` and `details`.
  */
-export const formatJsonError = ({ message, details }: HttpError) => {
-  return JSON.stringify({
-    status: 'error',
-    message,
-    details
-  });
+export const getJsonError = ({ status, message, details }: HttpError) => {
+  return {
+    status,
+    body: {
+      status: 'error',
+      message,
+      details
+    }
+  };
 };
