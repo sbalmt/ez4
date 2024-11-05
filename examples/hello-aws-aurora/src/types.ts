788172ebbf7dc7f7ebe971a73751f16d42993f82
Silas B <silas@balmante.eti.br>
2024-11-05 19:42:51 -0300

    enhance hello aurora example

--- examples/hello-aws-aurora/src/types.ts
+++ examples/hello-aws-aurora/src/types.ts
@@ -1,21 +1,51 @@
 import type { Database } from '@ez4/database';
+import type { String } from '@ez4/schema';
+
+export const enum EnumType {
+  A = 'a',
+  B = 'b'
+}
+
+export type ObjectType = {
+  foo: string;
+};
 
 /**
  * Table schema example.
  */
 export declare class TableSchema implements Database.Schema {
   /**
-   * Example of numeric property.
+   * Example of a numeric property.
    */
   id: number;
 
   /**
-   * Example of text property.
+   * Example of a text property.
    */
-  content: string;
+  text: string;
 
   /**
-   * Example of flag property
+   * Example of flag property.
    */
-  enabled: boolean;
+  bool: boolean;
+
+  /**
+   * Example of an enum property.
+   */
+  enum: EnumType;
+
+  /**
+   * Example of an object property.
+   */
+  object: ObjectType;
+
+  /**
+   * Example of a  property.
+   */
+  list: ObjectType[];
+
+  /**
+   * Example of a rich-type property (ISO date and time).
+   */
+  date: String.DateTime;
 }
