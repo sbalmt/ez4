a596c978da4cf28b411c4715dc10d0c4b9310dc5
Silas B <silas@balmante.eti.br>
2025-02-25 09:07:07 -0300

    fix: transform array from query string properly

--- packages/transform/src/schema/tuple.ts
+++ packages/transform/src/schema/tuple.ts
@@ -1,9 +1,18 @@
 import type { TupleSchema } from '@ez4/schema';
 
 import { getNewContext } from '../types/context.js';
+import { stringToArray } from '../utils/array.js';
 import { transformAny } from './any.js';
 
-export const transformTuple = (value: unknown, schema: TupleSchema, context = getNewContext()) => {
+export const transformTuple = (
+  value: unknown,
+  schema: TupleSchema,
+  context = getNewContext()
+): unknown[] | undefined => {
+  if (typeof value === 'string') {
+    return transformTuple(stringToArray(value), schema, context);
+  }
+
   if (!Array.isArray(value)) {
     return schema.definitions?.default;
   }
