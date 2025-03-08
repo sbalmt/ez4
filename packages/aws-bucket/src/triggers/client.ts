591444594f760f0ee6d63275b16f655860ffc5d3
Silas B <silas@balmante.eti.br>
2025-03-08 17:59:07 -0300

    refactor: add return type

--- packages/aws-bucket/src/triggers/client.ts
+++ packages/aws-bucket/src/triggers/client.ts
@@ -1,11 +1,11 @@
-import type { DeployOptions } from '@ez4/project/library';
+import type { DeployOptions, ExtraSource } from '@ez4/project/library';
 import type { BucketService } from '@ez4/storage/library';
 
 import { getServiceName } from '@ez4/project/library';
 import { createBucketStateId } from '../bucket/utils.js';
 import { getBucketName } from './utils.js';
 
-export const prepareLinkedClient = async (service: BucketService, options: DeployOptions) => {
+export const prepareLinkedClient = async (service: BucketService, options: DeployOptions): Promise<ExtraSource> => {
   const bucketName = await getBucketName(service, options);
   const bucketId = getServiceName(service, options);
   const stateId = createBucketStateId(bucketId);
