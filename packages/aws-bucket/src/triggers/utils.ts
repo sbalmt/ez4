e55b62033c0029bdcca6f318559b92a9c9a3334e
Silas B <silas@balmante.eti.br>
2025-03-10 23:40:10 -0300

    fix: keep prefix and project name due to permission issues

--- packages/aws-bucket/src/triggers/utils.ts
+++ packages/aws-bucket/src/triggers/utils.ts
@@ -7,13 +7,13 @@ import { toKebabCase } from '@ez4/utils';
 
 export const getBucketName = async (service: BucketService, options: DeployOptions) => {
   if (service.globalName) {
-    return service.globalName;
+    return getServiceName(service.globalName, options);
   }
 
-  const bucketName = getServiceName(service, options).substring(0, 46);
+  const bucketName = getServiceName(service, options);
   const randomName = await getRandomName(16);
 
-  return `${bucketName}-${randomName}`;
+  return `${bucketName.substring(0, 46)}-${randomName}`;
 };
 
 export const getInternalName = (service: BucketService, handlerName: string) => {
