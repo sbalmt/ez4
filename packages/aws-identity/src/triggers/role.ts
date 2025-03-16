d665ce7d6b2feb37d650b2fb532a71a103936a1e
Silas B <silas@balmante.eti.br>
2025-03-16 10:32:13 -0300

    fix: prevent multiple policies with same name to be attached

--- packages/aws-identity/src/triggers/role.ts
+++ packages/aws-identity/src/triggers/role.ts
@@ -1,12 +1,14 @@
 import type { RoleResourceEvent } from '@ez4/project/library';
+import type { EntryState } from '@ez4/stateful';
+import type { PolicyState } from '../policy/types.js';
 
 import { getServiceName } from '@ez4/project/library';
 
 import { createRole } from '../role/service.js';
 import { createPolicy } from '../policy/service.js';
+import { isPolicyState } from '../policy/utils.js';
 import { createPolicyDocument } from '../utils/policy.js';
 import { createRoleDocument } from '../utils/role.js';
-import { isPolicyState } from '../policy/utils.js';
 import { getAccountId } from '../utils/account.js';
 
 export const prepareExecutionRole = async (event: RoleResourceEvent) => {
@@ -16,7 +18,7 @@ export const prepareExecutionRole = async (event: RoleResourceEvent) => {
     return null;
   }
 
-  const policyList = policies.filter((policy) => isPolicyState(policy));
+  const policyList = preparePolicies(policies);
   const accountId = await getAccountId();
 
   const namePrefix = getServiceName('', options);
@@ -41,4 +43,21 @@ export const prepareExecutionRole = async (event: RoleResourceEvent) => {
       accountId
     )
   });
+};
+
+const preparePolicies = (policies: EntryState[]) => {
+  const policySet = new Set<string>();
+  const policyList: PolicyState[] = [];
+
+  for (const policy of policies) {
+    if (!isPolicyState(policy) || policySet.has(policy.parameters.policyName)) {
+      continue;
+    }
+
+    policySet.add(policy.parameters.policyName);
+
+    policyList.push(policy);
+  }
+
+  return policyList;
 };
