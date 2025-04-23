b2874015ae86fdea1804070e83f269d29a9605db
Silas B <silas@balmante.eti.br>
2025-04-23 19:22:02 -0300

    feat: export utils for browser compatibility

--- packages/utils/tools/bundler.mjs
+++ packages/utils/tools/bundler.mjs
@@ -3,3 +3,7 @@ import { bundlePackage } from '../../../tools/esbuild.mjs';
 // Default package.
 bundlePackage('src/main.ts', 'dist/main.mjs', 'esm');
 bundlePackage('src/main.ts', 'dist/main.cjs', 'cjs');
+
+// Browser package.
+bundlePackage('src/browser.ts', 'dist/browser.mjs', 'esm');
+bundlePackage('src/browser.ts', 'dist/browser.cjs', 'cjs');
