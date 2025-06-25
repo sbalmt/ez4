8ca74a39ee3f7d5a9f904719ba8b7ae2071b14f2
Silas B <silas@balmante.eti.br>
2025-06-24 21:47:18 -0300

    feat: wait for instance to resume when paused

--- packages/aws-aurora/src/client/common/client.ts
+++ packages/aws-aurora/src/client/common/client.ts
@@ -3,24 +3,123 @@ import type { AnyObject } from '@ez4/utils';
 import type { PreparedQueryCommand } from './queries.js';
 import type { Connection } from '../types.js';
 
+import { setTimeout } from 'node:timers/promises';
+
 import {
+  DatabaseResumingException,
   BeginTransactionCommand,
-  ExecuteStatementCommand,
-  CommitTransactionCommand,
   RollbackTransactionCommand,
+  CommitTransactionCommand,
+  ExecuteStatementCommand,
   RecordsFormatType,
   DecimalReturnType,
   LongReturnType
 } from '@aws-sdk/client-rds-data';
 
-export const beginTransaction = async (client: RDSDataClient, connection: Connection) => {
-  const result = await client.send(
-    new BeginTransactionCommand({
-      ...connection
-    })
-  );
+export type ExecuteOptions = {
+  transactionId?: string;
+  debug?: boolean;
+};
 
-  return result.transactionId!;
+export const executeStatement = async (
+  client: RDSDataClient,
+  connection: Connection,
+  command: PreparedQueryCommand,
+  options?: ExecuteOptions
+): Promise<AnyObject[]> => {
+  const transactionId = options?.transactionId;
+
+  try {
+    return await callWithRetryOnResume(async () => {
+      const { formattedRecords } = await client.send(
+        new ExecuteStatementCommand({
+          formatRecordsAs: RecordsFormatType.JSON,
+          resultSetOptions: {
+            decimalReturnType: DecimalReturnType.DOUBLE_OR_LONG,
+            longReturnType: LongReturnType.LONG
+          },
+          transactionId,
+          ...connection,
+          ...command
+        })
+      );
+
+      if (options?.debug) {
+        console.debug({
+          query: command.sql,
+          transaction: getDebugTransactionId(transactionId),
+          type: 'PgSQL'
+        });
+      }
+
+      if (formattedRecords) {
+        return JSON.parse(formattedRecords);
+      }
+
+      return [];
+    });
+  } catch (error) {
+    console.error({
+      query: command.sql,
+      transaction: getDebugTransactionId(transactionId),
+      type: 'PgSQL'
+    });
+
+    throw error;
+  }
+};
+
+export const executeStatements = async (
+  client: RDSDataClient,
+  connection: Connection,
+  commands: PreparedQueryCommand[],
+  options?: ExecuteOptions
+) => {
+  const results = [];
+
+  for (const command of commands) {
+    const result = await executeStatement(client, connection, command, options);
+
+    results.push(result);
+  }
+
+  return results;
+};
+
+export const executeTransaction = async (
+  client: RDSDataClient,
+  connection: Connection,
+  commands: PreparedQueryCommand[],
+  options?: ExecuteOptions
+) => {
+  const transactionId = await beginTransaction(client, connection);
+
+  try {
+    const results = await executeStatements(client, connection, commands, {
+      ...options,
+      transactionId
+    });
+
+    await commitTransaction(client, connection, transactionId);
+
+    return results;
+  } catch (error) {
+    await rollbackTransaction(client, connection, transactionId);
+
+    throw error;
+  }
+};
+
+export const beginTransaction = async (client: RDSDataClient, connection: Connection) => {
+  return callWithRetryOnResume(async () => {
+    const { transactionId } = await client.send(
+      new BeginTransactionCommand({
+        ...connection
+      })
+    );
+
+    return transactionId!;
+  });
 };
 
 export const rollbackTransaction = async (client: RDSDataClient, connection: Connection, transactionId: string) => {
@@ -41,90 +140,21 @@ export const commitTransaction = async (client: RDSDataClient, connection: Conne
   );
 };
 
-export const executeStatement = async (
-  client: RDSDataClient,
-  connection: Connection,
-  command: PreparedQueryCommand,
-  transactionId?: string,
-  debug?: boolean
-): Promise<AnyObject[]> => {
-  try {
-    if (debug) {
-      console.debug({
-        query: command.sql,
-        transaction: getDebugTransactionId(transactionId),
-        type: 'PgSQL'
-      });
-    }
-
-    const { formattedRecords } = await client.send(
-      new ExecuteStatementCommand({
-        formatRecordsAs: RecordsFormatType.JSON,
-        resultSetOptions: {
-          decimalReturnType: DecimalReturnType.DOUBLE_OR_LONG,
-          longReturnType: LongReturnType.LONG
-        },
-        transactionId,
-        ...connection,
-        ...command
-      })
-    );
-
-    if (formattedRecords) {
-      return JSON.parse(formattedRecords);
-    }
-  } catch (error) {
-    console.error({
-      query: command.sql,
-      transaction: getDebugTransactionId(transactionId),
-      type: 'PgSQL'
-    });
-
-    throw error;
-  }
-
-  return [];
-};
-
-export const executeStatements = async (
-  client: RDSDataClient,
-  connection: Connection,
-  commands: PreparedQueryCommand[],
-  transactionId?: string,
-  debug?: boolean
-) => {
-  const results = [];
-
-  for (const command of commands) {
-    const result = await executeStatement(client, connection, command, transactionId, debug);
-
-    results.push(result);
-  }
-
-  return results;
-};
-
-export const executeTransaction = async (
-  client: RDSDataClient,
-  connection: Connection,
-  commands: PreparedQueryCommand[],
-  debug?: boolean
-) => {
-  const transactionId = await beginTransaction(client, connection);
-
-  try {
-    const results = await executeStatements(client, connection, commands, transactionId, debug);
-
-    await commitTransaction(client, connection, transactionId);
-
-    return results;
-  } catch (error) {
-    await rollbackTransaction(client, connection, transactionId);
-
-    throw error;
-  }
-};
-
 const getDebugTransactionId = (transactionId: string | undefined) => {
   return transactionId?.substring(0, 8) ?? '-';
+};
+
+const callWithRetryOnResume = async <T>(callback: () => Promise<T>) => {
+  for (let milliseconds = 4500; ; milliseconds -= 500) {
+    try {
+      return await callback();
+    } catch (error) {
+      if (error instanceof DatabaseResumingException && milliseconds > 0) {
+        await setTimeout(milliseconds);
+        continue;
+      }
+
+      throw error;
+    }
+  }
 };
