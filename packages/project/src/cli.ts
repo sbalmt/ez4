a46155ca5a700714883962b27986fbad3f1f2ce7
Silas B <silas@balmante.eti.br>
2025-06-29 09:53:52 -0300

    refactor: allow typescript enums

--- packages/project/src/cli.ts
+++ packages/project/src/cli.ts
@@ -6,9 +6,26 @@ import { checkMinNodeVersion } from './terminal/version.js';
 
 checkMinNodeVersion();
 
-const appPath = join(import.meta.dirname, 'app.mjs');
-const extPath = join(import.meta.dirname, 'ext.mjs');
+const applicationPath = join(import.meta.dirname, 'app.mjs');
+const extensionsPath = join(import.meta.dirname, 'ext.mjs');
 
-spawn('node', ['--experimental-strip-types', '--no-warnings', '--loader', extPath, appPath, ...process.argv.slice(2)], {
-  stdio: 'inherit'
-});
+spawn(
+  'node',
+  [
+    // Enforce options
+    '--experimental-strip-types',
+    '--experimental-transform-types',
+    '--no-warnings',
+
+    // Handle file extensions
+    '--loader',
+    extensionsPath,
+
+    // Forward invocation
+    applicationPath,
+    ...process.argv.slice(2)
+  ],
+  {
+    stdio: 'inherit'
+  }
+);
