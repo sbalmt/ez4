0b7daf4a500a748753c843f79b2cb6adb576d55f
Silas B <silas@balmante.eti.br>
2025-07-21 19:04:55 -0300

    refactor: treat union, array and tuple types

--- packages/pgclient/src/utils/schema.ts
+++ packages/pgclient/src/utils/schema.ts
@@ -1,18 +1,13 @@
 import type { ValidationContextOptions } from '@ez4/validator';
 import type { AnySchema } from '@ez4/schema';
 
+import { isDynamicObjectSchema, isObjectSchema, isUnionSchema } from '@ez4/schema';
 import { validate, getUniqueErrorMessages, createValidatorContext } from '@ez4/validator';
 import { createTransformContext, transform } from '@ez4/transform';
 import { MalformedRequestError } from '@ez4/pgclient';
 
-export const validateSchemaWithContext = async (data: unknown, schema: AnySchema, context: Required<ValidationContextOptions>) => {
-  const errors = await validate(data, schema, createValidatorContext(context));
-
-  if (errors.length) {
-    const messages = getUniqueErrorMessages(errors);
-
-    throw new MalformedRequestError(context.property, messages);
-  }
+export const isDynamicFieldSchema = (schema: AnySchema) => {
+  return (isObjectSchema(schema) && isDynamicObjectSchema(schema)) || isUnionSchema(schema);
 };
 
 export const validateFirstSchemaLevel = (data: unknown, schema: AnySchema, path: string) => {
@@ -35,4 +30,14 @@ export const getWithSchemaValidation = async (data: unknown, schema: AnySchema, 
   await validateAllSchemaLevels(record, schema, path);
 
   return record;
+};
+
+const validateSchemaWithContext = async (data: unknown, schema: AnySchema, context: Required<ValidationContextOptions>) => {
+  const errors = await validate(data, schema, createValidatorContext(context));
+
+  if (errors.length) {
+    const messages = getUniqueErrorMessages(errors);
+
+    throw new MalformedRequestError(context.property, messages);
+  }
 };
