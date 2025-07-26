7b0e75fdf45ebf89ba6427542f7c7465614b885b
Silas B <silas@balmante.eti.br>
2025-07-26 11:06:20 -0300

    chore: add queue and notification utils tests

---
+++ packages/queue/test/utils.spec.ts
@@ -0,0 +1,49 @@
+import type { ObjectSchema } from '@ez4/schema';
+
+import { describe, it } from 'node:test';
+import { deepEqual } from 'node:assert';
+
+import { getJsonMessage, getJsonStringMessage } from '@ez4/queue/utils';
+import { SchemaType } from '@ez4/schema';
+
+describe('queue utils', () => {
+  it('assert :: get json message', async () => {
+    const messageSchema: ObjectSchema = {
+      type: SchemaType.Object,
+      properties: {
+        fooKey: {
+          type: SchemaType.String
+        }
+      }
+    };
+
+    const messageInput = {
+      fooKey: 'foo'
+    };
+
+    const outputOutput = await getJsonMessage(messageInput, messageSchema);
+
+    deepEqual(outputOutput, {
+      fooKey: 'foo'
+    });
+  });
+
+  it('assert :: get json message (string)', async () => {
+    const messageSchema: ObjectSchema = {
+      type: SchemaType.Object,
+      properties: {
+        fooKey: {
+          type: SchemaType.String
+        }
+      }
+    };
+
+    const messageInput = {
+      fooKey: 'foo'
+    };
+
+    const outputOutput = await getJsonStringMessage(messageInput, messageSchema);
+
+    deepEqual(outputOutput, JSON.stringify(messageInput));
+  });
+});
