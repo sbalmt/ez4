06b9c6ded57c7dc40fb8560aaf2318a3f52b0755
Silas B <silas@balmante.eti.br>
2025-08-02 09:01:50 -0300

    feat: enable local queues delay

--- providers/local/local-queue/src/service/client.ts
+++ providers/local/local-queue/src/service/client.ts
@@ -1,39 +1,46 @@
+import type { Client, Queue, SendOptions } from '@ez4/queue';
 import type { ServeOptions } from '@ez4/project/library';
 import type { MessageSchema } from '@ez4/queue/utils';
-import type { Client, Queue } from '@ez4/queue';
 
 import { getServiceName, Logger } from '@ez4/project/library';
 import { getJsonStringMessage } from '@ez4/queue/utils';
 
+export type ClientOptions = ServeOptions & {
+  delay?: number;
+};
+
 export const createQueueClient = <T extends Queue.Service<any>>(
   serviceName: string,
   messageSchema: MessageSchema,
-  serveOptions: ServeOptions
+  clientOptions: ClientOptions
 ): Client<T> => {
-  const queueIdentifier = getServiceName(serviceName, serveOptions);
-  const queueHost = `http://${serveOptions.serviceHost}/${queueIdentifier}`;
+  const queueIdentifier = getServiceName(serviceName, clientOptions);
+  const queueHost = `http://${clientOptions.serviceHost}/${queueIdentifier}`;
 
   return new (class {
-    async sendMessage(message: T['schema']) {
+    async sendMessage(message: T['schema'], options?: SendOptions<T>) {
       Logger.log(`✉️  Sending message to Queue [${serviceName}] at ${queueHost}`);
 
       const payload = await getJsonStringMessage(message, messageSchema);
+      const delay = clientOptions.delay ?? options?.delay ?? 0;
 
-      try {
-        const response = await fetch(queueHost, {
-          method: 'POST',
-          body: payload,
-          headers: {
-            ['content-type']: 'application/json'
+      setTimeout(async () => {
+        try {
+          const response = await fetch(queueHost, {
+            method: 'POST',
+            body: payload,
+            headers: {
+              ['content-type']: 'application/json'
+            }
+          });
+
+          if (!response.ok) {
+            Logger.error(`Queue [${serviceName}] isn't available.`);
           }
-        });
-
-        if (!response.ok) {
-          Logger.error(`Queue [${serviceName}] isn't available.`);
+        } catch (error) {
+          Logger.error(`${error}`);
         }
-      } catch (error) {
-        Logger.error(`${error}`);
-      }
+      }, delay * 1000);
     }
 
     async receiveMessage(): Promise<T['schema'][]> {
