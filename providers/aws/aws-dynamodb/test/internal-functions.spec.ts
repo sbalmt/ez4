13d9b4c6ea21816e2b6a32f75da9e49bab79d0df
Silas B <silas@balmante.eti.br>
2025-08-11 19:14:18 -0300

    fix: sort indexes before selection

---
+++ providers/aws/aws-dynamodb/test/internal-functions.spec.ts
@@ -0,0 +1,14 @@
+import { equal } from 'node:assert/strict';
+import { describe, it } from 'node:test';
+
+import { findBestSecondaryIndex } from '../src/client/common/indexes.js';
+
+describe('dynamodb internal functions', () => {
+  it('assert :: find best secondary index', () => {
+    const indexes = [['foo', 'bar'], ['foo'], ['bar']];
+
+    equal(findBestSecondaryIndex(indexes, { foo: 'foo' }), 'foo-index');
+    equal(findBestSecondaryIndex(indexes, { foo: 'foo', bar: 'bar' }), 'foo-bar-index');
+    equal(findBestSecondaryIndex(indexes, { bar: 'bar' }), 'bar-index');
+  });
+});
