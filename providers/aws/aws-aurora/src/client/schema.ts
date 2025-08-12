2ef48ebc2ea13f682ccbd6068301b861a4589d33
Silas B <silas@balmante.eti.br>
2025-08-12 19:08:24 -0300

    refactor: import common utils from pg client

--- providers/aws/aws-aurora/src/client/schema.ts
+++
@@ -1,7 +0,0 @@
-import type { AnySchema } from '@ez4/schema';
-
-import { isArraySchema, isObjectSchema, isTupleSchema, isUnionSchema } from '@ez4/schema';
-
-export const isJsonField = (schema: AnySchema) => {
-  return isObjectSchema(schema) || isUnionSchema(schema) || isArraySchema(schema) || isTupleSchema(schema);
-};
