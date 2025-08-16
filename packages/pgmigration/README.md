98c7ab13b6a1b80a60814c15f99a557c8f6fd934
Silas B <silas@balmante.eti.br>
2025-08-16 10:44:51 -0300

    chore: add pgmigration package

---
+++ packages/pgmigration/README.md
@@ -0,0 +1,15 @@
+# EZ4: PgMigration
+
+It provides a PostgreSQL migration library.
+
+## Getting started
+
+#### Install
+
+```sh
+npm install @ez4/pgmigration -D
+```
+
+## License
+
+MIT License
---
+++ packages/pgmigration/src/library.ts
@@ -0,0 +1,11 @@
+export * from './types/repository.js';
+export * from './types/engine.js';
+
+export * from './utils/repository.js';
+export * from './utils/resources.js';
+export * from './utils/schema.js';
+
+export * from './queries/insert.js';
+export * from './queries/update.js';
+export * from './queries/select.js';
+export * from './queries/delete.js';
