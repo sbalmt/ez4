643559054776e7d25362421af8d604aff00d35c9
Silas B <silas@balmante.eti.br>
2025-08-17 20:12:56 -0300

    chore: test extra table creation after renaming

--- packages/pgmigration/test/client-tables.spec.ts
+++ packages/pgmigration/test/client-tables.spec.ts
@@ -81,6 +81,30 @@ describe('migration :: client tables tests', async () => {
     }
   };
 
+  const repositoryV4: PgTableRepository = {
+    ...repositoryV3,
+    extra_table_c: {
+      name: 'extra_table_c',
+      relations: {},
+      schema: {
+        type: SchemaType.Object,
+        properties: {
+          id: {
+            type: SchemaType.String,
+            format: 'uuid'
+          }
+        }
+      },
+      indexes: {
+        id: {
+          type: Index.Primary,
+          columns: ['id'],
+          name: 'id'
+        }
+      }
+    }
+  };
+
   it('assert :: create tables', async () => {
     const queries = getCreateQueries(repositoryV1);
 
@@ -139,18 +163,44 @@ describe('migration :: client tables tests', async () => {
     ]);
   });
 
-  it('assert :: delete tables', async () => {
-    const queries = getDeleteQueries(repositoryV3);
+  it('assert :: extra tables', async () => {
+    const queries = getUpdateQueries(repositoryV4, repositoryV3);
 
     await runMigration(client, [...queries.tables, ...queries.indexes, ...queries.relations]);
 
     const result = await Promise.all([
       tableExists(client, 'renamed_table_a'),
       tableExists(client, 'table_b_renamed'),
+      tableExists(client, 'extra_table_c'),
       constraintExists(client, 'renamed_table_a_id_pk'),
-      constraintExists(client, 'table_b_renamed_id_pk')
+      constraintExists(client, 'table_b_renamed_id_pk'),
+      constraintExists(client, 'extra_table_c_id_pk')
     ]);
 
-    deepEqual(result, [[], [], [], []]);
+    deepEqual(result, [
+      [{ renamed_table_a: true }],
+      [{ table_b_renamed: true }],
+      [{ extra_table_c: true }],
+      [{ renamed_table_a_id_pk: true }],
+      [{ table_b_renamed_id_pk: true }],
+      [{ extra_table_c_id_pk: true }]
+    ]);
+  });
+
+  it('assert :: delete tables', async () => {
+    const queries = getDeleteQueries(repositoryV4);
+
+    await runMigration(client, [...queries.tables, ...queries.indexes, ...queries.relations]);
+
+    const result = await Promise.all([
+      tableExists(client, 'renamed_table_a'),
+      tableExists(client, 'table_b_renamed'),
+      tableExists(client, 'extra_table_c'),
+      constraintExists(client, 'renamed_table_a_id_pk'),
+      constraintExists(client, 'table_b_renamed_id_pk'),
+      constraintExists(client, 'extra_table_c_id_pk')
+    ]);
+
+    deepEqual(result, [[], [], [], [], [], []]);
   });
 });
