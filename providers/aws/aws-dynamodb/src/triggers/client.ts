031f1b4ea4f581b8b60ef2c064a9993d6cd2f980
Silas B <silas@balmante.eti.br>
2025-08-27 14:11:45 -0300

    fix: ensure primary key is always the first

--- providers/aws/aws-dynamodb/src/triggers/client.ts
+++ providers/aws/aws-dynamodb/src/triggers/client.ts
@@ -56,7 +56,9 @@ const getTableIndexes = (tableIndexes: TableIndex[]): string[][] => {
   const indexes = [];
 
   for (const { columns, type } of tableIndexes) {
-    if (type === Index.Primary || type === Index.Secondary) {
+    if (type === Index.Primary) {
+      indexes.unshift(columns);
+    } else if (type === Index.Secondary) {
       indexes.push(columns);
     }
   }
