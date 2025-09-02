a73209bfa81e56eff5358587c2df3c671c1c32de
Silas B <silas@balmante.eti.br>
2025-09-02 12:25:59 -0300

    refactor: add building flag to statements

--- libraries/pgsql/src/clauses/query/where.ts
+++ libraries/pgsql/src/clauses/query/where.ts
@@ -1,7 +1,31 @@
+import type { SqlBuilderOptions, SqlBuilderReferences } from '../../builder';
+import type { SqlSource } from '../../common/source';
+import type { SqlFilters } from '../../common/types';
+
 import { SqlConditions } from '../../operations/conditions';
 
 export class SqlWhereClause extends SqlConditions {
+  #state: {
+    source: SqlSource;
+    filters: SqlFilters;
+  };
+
+  constructor(source: SqlSource, references: SqlBuilderReferences, options: SqlBuilderOptions, filters: SqlFilters = {}) {
+    super(source, references, options, filters);
+
+    this.#state = {
+      source,
+      filters
+    };
+  }
+
   build(): [string, unknown[]] | undefined {
+    const { source } = this.#state;
+
+    if (!source.building) {
+      return source.build();
+    }
+
     const result = super.build();
 
     if (result) {
