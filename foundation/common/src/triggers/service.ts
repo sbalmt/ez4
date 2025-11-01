ac3678ef980258e021a0214d6e719caba024545d
Silas B <silas@balmante.eti.br>
2025-11-01 08:57:21 -0300

    chore: adjustments for local variables

--- foundation/common/src/triggers/service.ts
+++ foundation/common/src/triggers/service.ts
@@ -1,6 +1,9 @@
-import type { EmulatorService, MetadataServiceResult, PrepareResourceEvent, ServiceEvent } from '@ez4/project/library';
+import type { EmulateServiceEvent, PrepareResourceEvent, ServiceEvent } from '@ez4/project/library';
 import type { SourceMap } from '@ez4/reflection';
 
+import { getServiceName } from '@ez4/project/library';
+
+import { Client } from '../client';
 import { ServiceName, ServiceType } from '../types/services';
 import { prepareLinkedClient } from './client';
 import { isCommonService } from './utils';
@@ -13,7 +16,7 @@ export const prepareCommonServices = (event: PrepareResourceEvent) => {
   return isCommonService(event.service);
 };
 
-export const getCommonServices = (_reflection: SourceMap): MetadataServiceResult => {
+export const getCommonServices = (_reflection: SourceMap) => {
   return {
     errors: [],
     services: {
@@ -26,10 +29,19 @@ export const getCommonServices = (_reflection: SourceMap): MetadataServiceResult
   };
 };
 
-export const getCommonEmulators = (): EmulatorService => {
+export const getCommonEmulators = (event: EmulateServiceEvent) => {
+  const { service, options } = event;
+
+  if (!isCommonService(service)) {
+    return null;
+  }
+
+  const { name: serviceName } = service;
+
   return {
-    identifier: ServiceName,
-    name: ServiceName,
-    type: ServiceType
+    type: 'Common',
+    name: serviceName,
+    identifier: getServiceName(serviceName, options),
+    clientHandler: () => Client.make()
   };
 };
