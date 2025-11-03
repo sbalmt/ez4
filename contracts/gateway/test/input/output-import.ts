cbd548097f8741509eeda417a599a629ef75e3c9
Silas B <silas@balmante.eti.br>
2025-11-02 21:16:35 -0300

    chore: add import gateway tests

---
+++ contracts/gateway/test/input/output-import.ts
@@ -0,0 +1,35 @@
+import type { NamingStyle } from '@ez4/schema';
+import type { Http } from '@ez4/gateway';
+
+export declare class TestService extends Http.Service {
+  name: 'Remote API';
+
+  routes: [
+    {
+      path: 'ANY /test-route';
+      listener: typeof testListener;
+      handler: typeof testHandler;
+    }
+  ];
+
+  defaults: {
+    preferences: {
+      namingStyle: NamingStyle.Preserve;
+    };
+  };
+}
+
+function testListener(): void {}
+
+function testHandler(): Http.SuccessEmptyResponse {
+  return {
+    status: 204
+  };
+}
+
+/**
+ * Import API gateway.
+ */
+export declare class TestImport extends Http.Import<TestService> {
+  project: 'name from project in ez4.project.js';
+}
--- contracts/gateway/test/metadata.spec.ts
+++ contracts/gateway/test/metadata.spec.ts
@@ -1,27 +1,20 @@
-import { existsSync, readFileSync, writeFileSync } from 'node:fs';
-import { deepEqual, equal } from 'node:assert/strict';
+import { readFileSync, writeFileSync } from 'node:fs';
+import { deepEqual } from 'node:assert/strict';
 import { describe, it } from 'node:test';
 
-import { registerTriggers, getHttpServices } from '@ez4/gateway/library';
-import { getReflection } from '@ez4/project/library';
+import { registerTriggers } from '@ez4/gateway/library';
+import { getMetadata } from '@ez4/project/library';
 
 const testFile = (fileName: string, overwrite = false) => {
   const sourceFile = `./test/input/output-${fileName}.ts`;
   const outputFile = `./test/output/${fileName}.json`;
 
-  const reflection = getReflection([sourceFile]);
-  const result = getHttpServices(reflection);
+  const { metadata } = getMetadata([sourceFile]);
 
-  result.errors.forEach((error) => {
-    console.error(error.message);
-  });
-
-  equal(result.errors.length, 0);
-
-  if (overwrite || !existsSync(outputFile)) {
-    writeFileSync(outputFile, JSON.stringify(result.services, undefined, 2));
+  if (overwrite) {
+    writeFileSync(outputFile, JSON.stringify(metadata, undefined, 2));
   } else {
-    deepEqual(result.services, JSON.parse(readFileSync(outputFile).toString()));
+    deepEqual(metadata, JSON.parse(readFileSync(outputFile).toString()));
   }
 };
 
@@ -47,4 +40,5 @@ describe('http metadata', () => {
   it('assert :: route cache', () => testFile('cache'));
   it('assert :: route access', () => testFile('access'));
   it('assert :: route cors', () => testFile('cors'));
+  it('assert :: import api', () => testFile('import'));
 });
