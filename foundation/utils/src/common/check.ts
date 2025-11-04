bff8df1d896fb2c25ec2697a12452acee7ea32b0
Silas B <silas@balmante.eti.br>
2025-11-04 19:52:34 -0300

    feat: add is not nullish util

--- foundation/utils/src/common/check.ts
+++ foundation/utils/src/common/check.ts
@@ -1,8 +1,19 @@
 /**
  * Determines whether the given value is `null` or `undefined`.
+ *
  * @param value Value to check.
- * @returns Returns `true` when the object is nullish, `false` otherwise.
+ * @returns Returns `true` when the value is nullish, `false` otherwise.
  */
-export const isNullish = (value: unknown) => {
+export const isNullish = (value: unknown): value is null | undefined => {
   return value === undefined || value === null;
+};
+
+/**
+ * Determines whether the given value is not `null` or `undefined`.
+ *
+ * @param value Value to check.
+ * @returns Returns `true` when the value is not nullish, `false` otherwise.
+ */
+export const isNotNullish = <T>(value: T): value is Exclude<T, null | undefined> => {
+  return value !== undefined && value !== null;
 };
