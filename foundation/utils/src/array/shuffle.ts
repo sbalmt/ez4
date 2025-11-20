436c5f9392b57986ea33348d240ca9468cc63828
Silas B <silas@balmante.eti.br>
2025-11-19 21:53:24 -0300

    feat: add array shuffle util

---
+++ foundation/utils/src/array/shuffle.ts
@@ -0,0 +1,20 @@
+import { getRandomInteger } from '../browser';
+
+/**
+ * Copy and shuffle the given array.
+ *
+ * @param array Input array.
+ * @returns Returns the new shuffled array.
+ */
+export const arrayShuffle = <T>(array: T[]) => {
+  const length = array.length - 1;
+  const result = [...array];
+
+  for (let sourceIndex = length - 1; sourceIndex > 0; sourceIndex--) {
+    const targetIndex = getRandomInteger(0, length);
+
+    [result[sourceIndex], result[targetIndex]] = [result[targetIndex], result[sourceIndex]];
+  }
+
+  return result;
+};
