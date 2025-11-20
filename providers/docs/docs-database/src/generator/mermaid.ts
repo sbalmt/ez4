5891740ba2259961249db38c733ce474e1b201a7
Silas B <silas@balmante.eti.br>
2025-11-20 14:06:02 -0300

    chore: small adjustments

--- providers/docs/docs-database/src/generator/mermaid.ts
+++ providers/docs/docs-database/src/generator/mermaid.ts
@@ -3,123 +3,125 @@ import type { ObjectSchemaProperties } from '@ez4/schema';
 
 import { Index } from '@ez4/database/library';
 
-export const getDatabaseOutput = (databaseService: DatabaseService) => {
-  const output = ['erDiagram'];
+export namespace MermaidGenerator {
+  export const getDatabaseOutput = (databaseService: DatabaseService) => {
+    const output = ['erDiagram'];
 
-  for (const table of databaseService.tables) {
-    const { schema, indexes, relations = [] } = table;
+    for (const table of databaseService.tables) {
+      const { schema, indexes, relations = [] } = table;
 
-    const allRelations = getRelationsOutput(table.name, databaseService.tables, schema.properties, relations);
-    const allColumns = getColumnsOutput(schema.properties, relations, indexes);
+      const allRelations = getRelationsOutput(table.name, databaseService.tables, schema.properties, relations);
+      const allColumns = getColumnsOutput(schema.properties, relations, indexes);
 
-    output.push(`\t${table.name} {`);
-    output.push(allColumns);
-    output.push('\t}');
+      output.push(`\t${table.name} {`);
+      output.push(allColumns);
+      output.push('\t}');
 
-    if (allRelations.length > 0) {
-      output.push(allRelations);
-    }
-  }
-
-  return output.join('\n');
-};
-
-export const getColumnsOutput = (columns: ObjectSchemaProperties, relations: TableRelation[], indexes: TableIndex[]) => {
-  const output = [];
-
-  for (const name in columns) {
-    const relation = relations.find((relation) => relation.targetColumn === name);
-    const index = indexes.find((index) => index.columns.includes(name));
-
-    const schema = columns[name];
-    const column = [name, schema.type];
-
-    const indexType = relation?.sourceIndex;
-
-    const attributes = [];
-
-    if (indexType === Index.Primary || indexType === Index.Unique) {
-      attributes.push('FK');
+      if (allRelations.length > 0) {
+        output.push(allRelations);
+      }
     }
 
-    if (index?.type === Index.Primary) {
-      attributes.push('PK');
-    }
-
-    if (index?.type === Index.Unique) {
-      attributes.push('UK');
-    }
-
-    if (attributes.length > 0) {
-      column.push(attributes.join(','));
-    }
-
-    output.push(`\t\t${column.join(' ')}`);
-  }
-
-  return output.join('\n');
-};
-
-export const getRelationsOutput = (
-  targetTable: string,
-  tables: DatabaseTable[],
-  columns: ObjectSchemaProperties,
-  relations: TableRelation[]
-) => {
-  const output = [];
-
-  const getConnectionLabel = (tableName: string, columnName: string) => {
-    return `${tableName}(${columnName})`;
+    return output.join('\n');
   };
 
-  for (const relation of relations) {
-    const { sourceTable, sourceColumn, targetColumn, sourceIndex, targetIndex } = relation;
+  export const getColumnsOutput = (columns: ObjectSchemaProperties, relations: TableRelation[], indexes: TableIndex[]) => {
+    const output = [];
 
-    const sourceTableData = tables.find((table) => table.name === sourceTable);
+    for (const name in columns) {
+      const relation = relations.find((relation) => relation.targetColumn === name);
+      const index = indexes.find((index) => index.columns.includes(name));
 
-    const sourceLabel = getConnectionLabel(sourceTable, sourceColumn);
-    const targetLabel = getConnectionLabel(targetTable, targetColumn);
+      const schema = columns[name];
+      const column = [name, schema.type];
 
-    const isSourceNullable = sourceTableData?.schema.properties[sourceColumn]?.nullable;
-    const isTargetNullable = columns[targetColumn]?.nullable;
+      const indexType = relation?.sourceIndex;
 
-    const sourceConnector = `${isSourceNullable ? 'o' : '|'}`;
-    const targetConnector = `${isTargetNullable ? 'o' : '|'}`;
+      const attributes = [];
 
-    const connection = [];
-
-    if (targetIndex === Index.Primary || targetIndex === Index.Unique) {
-      connection.push(targetTable);
-
-      if (sourceIndex === Index.Primary || sourceIndex === Index.Unique) {
-        connection.push(`|${targetConnector}--${sourceConnector}|`);
-      } else {
-        connection.push(`|${targetConnector}--o{`);
+      if (indexType === Index.Primary || indexType === Index.Unique) {
+        attributes.push('FK');
       }
 
-      connection.push(sourceTable, ':', `"${targetLabel} → ${sourceLabel}"`);
-    } else {
-      const hasOtherSideRelation = sourceTableData?.relations?.some((relation) => {
-        return relation.sourceTable === targetTable && relation.sourceColumn === targetColumn;
-      });
+      if (index?.type === Index.Primary) {
+        attributes.push('PK');
+      }
 
-      if (!hasOtherSideRelation) {
-        connection.push(sourceTable);
+      if (index?.type === Index.Unique) {
+        attributes.push('UK');
+      }
+
+      if (attributes.length > 0) {
+        column.push(attributes.join(','));
+      }
+
+      output.push(`\t\t${column.join(' ')}`);
+    }
+
+    return output.join('\n');
+  };
+
+  export const getRelationsOutput = (
+    targetTable: string,
+    tables: DatabaseTable[],
+    columns: ObjectSchemaProperties,
+    relations: TableRelation[]
+  ) => {
+    const output = [];
+
+    const getConnectionLabel = (tableName: string, columnName: string) => {
+      return `${tableName}(${columnName})`;
+    };
+
+    for (const relation of relations) {
+      const { sourceTable, sourceColumn, targetColumn, sourceIndex, targetIndex } = relation;
+
+      const sourceTableData = tables.find((table) => table.name === sourceTable);
+
+      const sourceLabel = getConnectionLabel(sourceTable, sourceColumn);
+      const targetLabel = getConnectionLabel(targetTable, targetColumn);
+
+      const isSourceNullable = sourceTableData?.schema.properties[sourceColumn]?.nullable;
+      const isTargetNullable = columns[targetColumn]?.nullable;
+
+      const sourceConnector = `${isSourceNullable ? 'o' : '|'}`;
+      const targetConnector = `${isTargetNullable ? 'o' : '|'}`;
+
+      const connection = [];
+
+      if (targetIndex === Index.Primary || targetIndex === Index.Unique) {
+        connection.push(targetTable);
 
         if (sourceIndex === Index.Primary || sourceIndex === Index.Unique) {
-          connection.push(`|${sourceConnector}--${targetConnector}{`);
+          connection.push(`|${targetConnector}--${sourceConnector}|`);
         } else {
-          connection.push(`|${sourceConnector}--o{`);
+          connection.push(`|${targetConnector}--o{`);
         }
 
-        connection.push(targetTable, ':', `"${sourceLabel} → ${targetLabel}"`);
+        connection.push(sourceTable, ':', `"${targetLabel} → ${sourceLabel}"`);
+      } else {
+        const hasOtherSideRelation = sourceTableData?.relations?.some((relation) => {
+          return relation.sourceTable === targetTable && relation.sourceColumn === targetColumn;
+        });
+
+        if (!hasOtherSideRelation) {
+          connection.push(sourceTable);
+
+          if (sourceIndex === Index.Primary || sourceIndex === Index.Unique) {
+            connection.push(`|${sourceConnector}--${targetConnector}{`);
+          } else {
+            connection.push(`|${sourceConnector}--o{`);
+          }
+
+          connection.push(targetTable, ':', `"${sourceLabel} → ${targetLabel}"`);
+        }
+      }
+
+      if (connection.length > 0) {
+        output.push(`\t${connection.join(' ')}`);
       }
     }
 
-    if (connection.length > 0) {
-      output.push(`\t${connection.join(' ')}`);
-    }
-  }
-
-  return output.join('\n');
-};
+    return output.join('\n');
+  };
+}
--- providers/docs/docs-database/src/provider/generator.ts
+++ providers/docs/docs-database/src/provider/generator.ts
@@ -1,4 +1,4 @@
-import type { GenerateResourceEvent } from '@ez4/project/library';
+import type { GenerateResourceEvent, MetadataReflection } from '@ez4/project/library';
 
 import { Logger } from '@ez4/project/library';
 import { toKebabCase } from '@ez4/utils';
@@ -6,7 +6,7 @@ import { toKebabCase } from '@ez4/utils';
 import { writeFile } from 'node:fs/promises';
 import { join } from 'node:path';
 
-import { getDatabaseOutput } from '../generator/mermaid';
+import { MermaidGenerator } from '../generator/mermaid';
 import { getDatabaseServices } from '../utils/service';
 
 export const generateResource = async (event: GenerateResourceEvent) => {
@@ -16,20 +16,23 @@ export const generateResource = async (event: GenerateResourceEvent) => {
 
   switch (command) {
     case 'database:erd': {
-      const databases = getDatabaseServices(metadata);
-
-      for (const database of databases) {
-        const outputFile = join(outputPath, `${toKebabCase(database.name)}-erd.mmd`);
-        const outputContent = getDatabaseOutput(database);
-
-        await writeFile(outputFile, outputContent);
-
-        Logger.success(`ERD saved to ${outputFile}`);
-      }
-
+      await generateErd(outputPath, metadata);
       return true;
     }
   }
 
   return null;
+};
+
+const generateErd = async (outputPath: string, metadata: MetadataReflection) => {
+  const databases = getDatabaseServices(metadata);
+
+  for (const database of databases) {
+    const outputFile = join(outputPath, `${toKebabCase(database.name)}-erd.mmd`);
+    const outputContent = MermaidGenerator.getDatabaseOutput(database);
+
+    await writeFile(outputFile, outputContent);
+
+    Logger.success(`Database ERD written to ${outputFile}`);
+  }
 };
