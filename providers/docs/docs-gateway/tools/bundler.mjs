b1733cad133509a4274b8a2cb4c48427d868e541
Wellington Martins <wellingtonsdsm@gmail.com>
2025-12-06 19:54:08 -0300

    feat(docs): use docs-gateway generator runtime instead of local oas.yml file

--- providers/docs/docs-gateway/tools/bundler.mjs
+++ providers/docs/docs-gateway/tools/bundler.mjs
@@ -3,3 +3,7 @@ import { bundlePackage } from '../../../../tools/esbuild.mjs';
 // Default package.
 bundlePackage('src/main.ts', 'dist/main.mjs', 'esm');
 bundlePackage('src/main.ts', 'dist/main.cjs', 'cjs');
+
+// Library package.
+bundlePackage('src/library.ts', 'dist/library.mjs', 'esm');
+bundlePackage('src/library.ts', 'dist/library.cjs', 'cjs');
