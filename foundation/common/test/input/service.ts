a0f5143eaa1983a7845e1a8f28d1b6822b4231a7
Silas B <silas@balmante.eti.br>
2025-12-18 12:29:03 -0300

    fix: add missing test client

--- foundation/common/test/input/service.ts
+++ foundation/common/test/input/service.ts
@@ -1,6 +1,8 @@
 import type { Environment } from '@ez4/common';
 
-export declare class TestService {}
+export declare class TestService {
+  client: never;
+}
 
 export declare class ServiceCommonTest {
   services: {
