e401731a32c375d2b902198e4df79de0a04f1b86
Silas B <silas@balmante.eti.br>
2026-01-17 16:32:02 -0300

    chore: log space

--- foundation/project/src/utils/errors.ts
+++ foundation/project/src/utils/errors.ts
@@ -3,6 +3,7 @@ import { Logger } from '@ez4/project/library';
 export const assertNoErrors = (errors: Error[]) => {
   if (errors.length) {
     errors.forEach((error) => Logger.error(error.message));
+    Logger.space();
 
     throw new Error('No errors were expected.');
   }
