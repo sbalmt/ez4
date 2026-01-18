462bd3abba6db7933358547ee60c80e2b27608a3
Silas B <silas@balmante.eti.br>
2026-01-18 17:06:17 -0300

    chore: isolate aws operation logger type

--- providers/aws/aws-common/src/common/logger.ts
+++ providers/aws/aws-common/src/common/logger.ts
@@ -1,9 +1,11 @@
 import { Color, Logger as ProjectLogger, toBold, toColor, toRed } from '@ez4/project/library';
 
 export namespace Logger {
-  export type OperationLogger = ProjectLogger.LogLine;
-
   export type Callback<T> = (logger: OperationLogger) => T | Promise<T>;
+
+  export type OperationLogger = {
+    update: (message: string) => void;
+  };
 
   export const logOperation = async <T>(serviceName: string, resource: string, operation: string, callback: Callback<T>) => {
     const logger = ProjectLogger.logLine(`▶️  Starting ${toBold(resource)} ${operation} ${toColor(Color.BrightBlack, `[${serviceName}]`)}`);
