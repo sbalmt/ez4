4e872a49278ff2ded77467e1f086d1d349ec29a2
Silas B <silas@balmante.eti.br>
2026-02-27 20:36:09 -0300

    chore: add index migration tests

---
+++ libraries/pgmigration/test/client-index.spec.ts
@@ -0,0 +1,128 @@
+import type { PgTableRepository } from '@ez4/pgclient/library';
+
+import { describe, it } from 'node:test';
+import { deepEqual } from 'assert/strict';
+
+import { getCreateQueries, getDeleteQueries, getUpdateQueries } from '@ez4/pgmigration';
+import { Client } from '@ez4/pgclient';
+import { SchemaType } from '@ez4/schema';
+import { Index } from '@ez4/database';
+
+import { constraintExists, indexExists, tableExists } from './common/queries';
+import { runMigration } from './common/migration';
+
+describe('migration :: client index tests', async () => {
+  const client = Client.make({
+    debug: false,
+    repository: {},
+    connection: {
+      database: 'postgres',
+      password: 'postgres',
+      user: 'postgres',
+      host: '127.0.0.1'
+    }
+  });
+
+  const repositoryV1: PgTableRepository = {
+    table: {
+      name: 'table',
+      relations: {},
+      schema: {
+        type: SchemaType.Object,
+        properties: {
+          id: {
+            type: SchemaType.String,
+            format: 'uuid'
+          },
+          column: {
+            type: SchemaType.String
+          }
+        }
+      },
+      indexes: {
+        id: {
+          type: Index.Primary,
+          columns: ['id'],
+          name: 'id'
+        }
+      }
+    }
+  };
+
+  const repositoryV2: PgTableRepository = {
+    table: {
+      ...repositoryV1.table,
+      indexes: {
+        ...repositoryV1.table.indexes,
+        column: {
+          type: Index.Secondary,
+          columns: ['column'],
+          name: 'column'
+        }
+      }
+    }
+  };
+
+  const repositoryV3: PgTableRepository = {
+    renamed_table: {
+      ...repositoryV2.table,
+      name: 'renamed_table'
+    }
+  };
+
+  it('assert :: create index', async () => {
+    const queries = getCreateQueries(repositoryV1);
+
+    await runMigration(client, queries);
+
+    const result = await Promise.all([
+      tableExists(client, 'table'),
+      constraintExists(client, 'table_id_pk'),
+      indexExists(client, 'table_column_sk')
+    ]);
+
+    deepEqual(result, [[{ table: true }], [{ table_id_pk: true }], []]);
+  });
+
+  it('assert :: update index', async () => {
+    const queries = getUpdateQueries(repositoryV2, repositoryV1);
+
+    await runMigration(client, queries);
+
+    const result = await Promise.all([
+      tableExists(client, 'table'),
+      constraintExists(client, 'table_id_pk'),
+      indexExists(client, 'table_column_sk')
+    ]);
+
+    deepEqual(result, [[{ table: true }], [{ table_id_pk: true }], [{ table_column_sk: true }]]);
+  });
+
+  it('assert :: rename tables', async () => {
+    const queries = getUpdateQueries(repositoryV3, repositoryV2);
+
+    await runMigration(client, queries);
+
+    const result = await Promise.all([
+      tableExists(client, 'renamed_table'),
+      constraintExists(client, 'renamed_table_id_pk'),
+      indexExists(client, 'renamed_table_column_sk')
+    ]);
+
+    deepEqual(result, [[{ renamed_table: true }], [{ renamed_table_id_pk: true }], [{ renamed_table_column_sk: true }]]);
+  });
+
+  it('assert :: delete tables', async () => {
+    const queries = getDeleteQueries(repositoryV3);
+
+    await runMigration(client, queries);
+
+    const result = await Promise.all([
+      tableExists(client, 'renamed_table'),
+      constraintExists(client, 'renamed_table_id_pk'),
+      indexExists(client, 'renamed_table_column_sk')
+    ]);
+
+    deepEqual(result, [[], [], []]);
+  });
+});
