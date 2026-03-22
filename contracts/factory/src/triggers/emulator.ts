ae687069b12bcd312e58ab5677225d284a10eaf4
Silas B <silas@balmante.eti.br>
2026-03-22 12:02:17 -0300

    refactor: split services and emulator

---
+++ contracts/factory/src/triggers/emulator.ts
@@ -0,0 +1,35 @@
+import type { EmulateServiceEvent } from '@ez4/project/library';
+
+import { getServiceName, createEmulatorModule } from '@ez4/project/library';
+
+import { isFactoryService } from '../metadata/types';
+
+export const getEmulatorService = (event: EmulateServiceEvent) => {
+  const { service, options, context } = event;
+
+  if (!isFactoryService(service)) {
+    return null;
+  }
+
+  const { name: serviceName, services, handler } = service;
+
+  return {
+    type: 'Factory',
+    name: serviceName,
+    identifier: getServiceName(serviceName, options),
+    exportHandler: async () => {
+      const clients = await context.makeClients(services);
+
+      const factoryModule = await createEmulatorModule({
+        version: options.version,
+        entrypoint: handler,
+        variables: {
+          ...options.variables,
+          ...service.variables
+        }
+      });
+
+      return factoryModule.invoke(clients);
+    }
+  };
+};
--- contracts/factory/src/triggers/service.ts
+++ contracts/factory/src/triggers/service.ts
@@ -1,7 +1,6 @@
-import type { EmulateServiceEvent, PrepareResourceEvent, ServiceEvent } from '@ez4/project/library';
+import type { PrepareResourceEvent, ServiceEvent } from '@ez4/project/library';
 import type { TypeClass } from '@ez4/reflection';
 
-import { getServiceName, createEmulatorModule } from '@ez4/project/library';
 import { createVirtualState } from '@ez4/common/library';
 
 import { isFactoryServiceDeclaration } from '../metadata/service';
@@ -31,34 +30,4 @@ export const prepareResources = (event: PrepareResourceEvent) => {
   }
 
   return false;
-};
-
-export const getEmulatorService = (event: EmulateServiceEvent) => {
-  const { service, options, context } = event;
-
-  if (!isFactoryService(service)) {
-    return null;
-  }
-
-  const { name: serviceName, services, handler } = service;
-
-  return {
-    type: 'Factory',
-    name: serviceName,
-    identifier: getServiceName(serviceName, options),
-    exportHandler: async () => {
-      const clients = await context.makeClients(services);
-
-      const factoryModule = await createEmulatorModule({
-        version: options.version,
-        entrypoint: handler,
-        variables: {
-          ...options.variables,
-          ...service.variables
-        }
-      });
-
-      return factoryModule.invoke(clients);
-    }
-  };
 };
