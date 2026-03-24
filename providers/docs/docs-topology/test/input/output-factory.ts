91d98090d2b72b9fb64cd28a4e7d3ba087ad6b27
Silas B <silas@balmante.eti.br>
2026-03-24 09:32:00 -0300

    fix: adjustments after tests

--- providers/docs/docs-topology/test/input/output-factory.ts
+++ providers/docs/docs-topology/test/input/output-factory.ts
@@ -1,7 +1,17 @@
 import type { Factory } from '@ez4/factory';
 
-export declare class TestFactory extends Factory.Service<void> {
+interface TestService {
+  helloWorld(): void;
+}
+
+export declare class TestFactory extends Factory.Service<TestService> {
   handler: typeof factory;
 }
 
-export function factory() {}
+export function factory() {
+  return {
+    helloWorld: () => {
+      // do nothing...
+    }
+  };
+}
