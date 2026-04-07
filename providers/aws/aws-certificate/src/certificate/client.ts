20bef0c59f7b7bc20c98056ab7048a3c393b9bae
Silas B <silas@balmante.eti.br>
2026-04-07 20:09:19 -0300

    chore: log manual validation step

--- providers/aws/aws-certificate/src/certificate/client.ts
+++ providers/aws/aws-certificate/src/certificate/client.ts
@@ -25,7 +25,7 @@ export type CreateResponse = {
 };
 
 export const isCertificateInUse = async (logger: OperationLogLine, certificateArn: string) => {
-  logger.update(`Fetching  certificate`);
+  logger.update(`Fetching certificate`);
 
   const response = await getACMClient().send(
     new DescribeCertificateCommand({
@@ -55,6 +55,8 @@ export const createCertificate = async (logger: OperationLogLine, request: Creat
   );
 
   const certificateArn = response.CertificateArn as Arn;
+
+  logger.update(`Waiting manual validation`);
 
   await waitUntilCertificateValidated(getACMWaiter(client), {
     CertificateArn: certificateArn
