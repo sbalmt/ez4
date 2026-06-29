1955fe6fb56e10d681c791401f037b371b69f841
Silas B <silas@balmante.eti.br>
2026-06-28 23:57:54 -0300

    chore: use better names

--- extensions/vscode/src/webview/components/editor/workers.ts
+++ extensions/vscode/src/webview/components/editor/workers.ts
@@ -7,23 +7,25 @@ export const registerEditorWorkers = () => {
   self.MonacoEnvironment = {
     getWorker: async (_, name) => {
       const script = document.querySelector<HTMLScriptElement>('script[src*="webview.js"]');
-      const base = script?.src.substring(0, script.src.lastIndexOf('/'));
+      const source = script?.src.substring(0, script.src.lastIndexOf('/'));
 
       const file = WORKER_NAME_MAP[name] ?? name;
-      const url = `${base}/${file}.worker.js`;
+      const path = `${source}/${file}.worker.js`;
 
       //! IMPORTANT: None of those options worked so far.
       //! - Loading *.worker.js inside WSL with/without baseUrl (failed).
       //! - importScripts and await import inside worker blob (failed).
-      const response = await fetch(url);
+      const response = await fetch(path);
 
       if (!response.ok) {
-        throw new Error(`Unable to load worker script: ${url}`);
+        throw new Error(`Unable to load worker script: ${path}`);
       }
 
       const code = await response.text();
+      const blob = new Blob([code], { type: 'application/javascript' });
+      const data = URL.createObjectURL(blob);
 
-      return new Worker(URL.createObjectURL(new Blob([code], { type: 'application/javascript' })));
+      return new Worker(data);
     }
   };
 };
