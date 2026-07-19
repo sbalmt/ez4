72386909a3b005a568a10093fdf97d5af81656f3
Silas B <silas@balmante.eti.br>
2026-07-19 10:43:29 -0300

    chore: simplify atomic relaceWith type

--- contracts/database/src/services/query/atomic.ts
+++ contracts/database/src/services/query/atomic.ts
@@ -1,33 +1,21 @@
-import type { AnyObject, IsUndefined, IsObject } from '@ez4/utils';
+import type { AnyObject, IsObject } from '@ez4/utils';
 import type { PreserveNull } from './utils';
 
 export type AtomicFields<T extends AnyObject> = {
-  [P in keyof T]?: AtomicField<T[P], false>;
+  [P in keyof T]?: AtomicField<T[P]>;
 };
 
-export type AtomicStrictFields<T extends AnyObject> = AtomicRequiredFields<T, true> & AtomicOptionalFields<T, true>;
-
-type AtomicField<T, S> = T extends number
+type AtomicField<T> = T extends number
   ? AtomicIncrement | AtomicDecrement | AtomicMultiply | AtomicDivide | T
   : IsObject<T> extends true
-    ? S extends true
-      ? PreserveNull<T, AtomicStrictFields<NonNullable<T>>>
-      : PreserveNull<T, AtomicFields<NonNullable<T>> | AtomicReplaceWith<NonNullable<T>>>
+    ? PreserveNull<T, AtomicFields<NonNullable<T>> | AtomicReplaceWith<NonNullable<T>>>
     : T;
-
-type AtomicRequiredFields<T extends AnyObject, S> = {
-  [P in keyof T as IsUndefined<T[P]> extends true ? never : P]: AtomicField<T[P], S>;
-};
-
-type AtomicOptionalFields<T extends AnyObject, S> = {
-  [P in keyof T as IsUndefined<T[P]> extends true ? P : never]?: AtomicField<T[P], S>;
-};
 
 type AtomicReplaceWith<T extends AnyObject> = {
   /**
    * Replace the entry value with the given object.
    */
-  replaceWith: AtomicStrictFields<T>;
+  replaceWith: T;
 };
 
 type AtomicIncrement = {
