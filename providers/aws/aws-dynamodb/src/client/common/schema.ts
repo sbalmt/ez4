6308c371da3fd07ada6b29818acdc06201ce3c5c
Silas B <silas@balmante.eti.br>
2026-07-20 10:34:35 -0300

    feat: add support for default values in dynamodb

--- providers/aws/aws-dynamodb/src/client/common/schema.ts
+++ providers/aws/aws-dynamodb/src/client/common/schema.ts
@@ -1,9 +1,11 @@
 import type { AnySchema, ObjectSchema } from '@ez4/schema';
+import type { AnyObject } from '@ez4/utils';
 
 import { isDynamicObjectSchema, isObjectSchema } from '@ez4/schema';
 import { validate, createValidatorContext, getErrorDetails } from '@ez4/validator';
 import { createTransformContext, transform } from '@ez4/transform';
 import { MalformedRequestError } from '@ez4/aws-dynamodb/runtime';
+import { isAnyObject, deepClone } from '@ez4/utils';
 
 export const isDynamicFieldSchema = (schema: AnySchema): schema is ObjectSchema => {
   return isObjectSchema(schema) && isDynamicObjectSchema(schema);
@@ -24,4 +26,16 @@ export const getWithSchemaValidation = async <T>(data: unknown, schema: AnySchem
   await validateRecordSchema(record, schema, path);
 
   return record as T;
+};
+
+export const getTransformedRecords = <T extends AnyObject>(records: T[], schema: AnySchema, select?: AnyObject) => {
+  return records.map((record) => {
+    const result = transform(record, schema);
+
+    if (isAnyObject(result)) {
+      return deepClone(result, { include: select }) as T;
+    }
+
+    return result as T;
+  });
 };
