555edc3f31e6e1312cfa05d3f015a7f27179b4ae
Silas Balmante <silas@balmante.tech>
2026-09-10 21:54:33 -0300

    fix: minor gaps in the storage contract

--- contracts/storage/src/metadata/handler.ts
+++ contracts/storage/src/metadata/handler.ts
@@ -22,7 +22,7 @@ export const getEventHandlerMetadata = (type: AllType, errorList: Error[]) => {
   }
 
   if (type.parameters) {
-    const [contextType] = type.parameters;
+    const [, contextType] = type.parameters;
 
     if (contextType) {
       const references = getFunctionReferences(contextType);
